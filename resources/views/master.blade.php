915cf17d02482724b51f599532819781619c8693
Jurgen Treep <jurgen.treep@gmail.com>
2016-04-20 19:47:50 +0200

    some styling mainly for the typography

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,7 +7,7 @@
         <meta name="HandheldFriendly" content="True">
         <meta name="MobileOptimized" content="320">
         <title>{{ $article->title }} - Wait But Why Reader</title>
-        <link href='https://fonts.googleapis.com/css?family=Noto+Sans:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Noto+Serif:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
         <link href='/css/master.css' rel='stylesheet' type='text/css'>
     </head>
     <body>
